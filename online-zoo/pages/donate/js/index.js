956c9faeede7f44c016fb0eb1cb055fcd0bdf5be
Bohdan <Bohdansherbak@gmail.com>
2024-04-12 02:13:18 +0300

    circle

--- online-zoo/pages/donate/js/index.js
+++ online-zoo/pages/donate/js/index.js
@@ -5,20 +5,31 @@ const rangeValueLabel = document.getElementById('form__value');
 const costNumb = document.querySelectorAll('.cost__number')
 const button = document.querySelector('.button');
 
+function reetCircle(){
+  circle.forEach(e =>{
+        let idd = e.id;
+    e.classList.remove('hoverCircle');
+    costNumb[idd].classList.remove('hoverCost')
+  })
+}
 circle.forEach(e => {
+      let idd = e.id;
   e.addEventListener('click', () => {
     rangeValueLabel.value = e.title;
-  })
-
-  e.addEventListener('mouseover', () => {
-    let idd = e.id;
+    reetCircle()
     e.classList.add('hoverCircle');
     costNumb[idd].classList.add('hoverCost')
   })
 
-  e.addEventListener('mouseout', () => {
-    let idd = e.id;
-    e.classList.remove('hoverCircle');
-    costNumb[idd].classList.remove('hoverCost')
-  })
+  // e.addEventListener('mouseover', () => {
+  //   let idd = e.id;
+  //   e.classList.add('hoverCircle');
+  //   costNumb[idd].classList.add('hoverCost')
+  // })
+
+  // e.addEventListener('mouseout', () => {
+  //   let idd = e.id;
+  //   e.classList.remove('hoverCircle');
+  //   costNumb[idd].classList.remove('hoverCost')
+  // })
 });
